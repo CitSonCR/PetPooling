a37cf43568f08aff227b7040f61477e141765085
Marlon <mrojasc490@ulacit.ed.cr>
2018-08-15 13:16:22 -0600

    Android App

--- PetPooling/PetPooling/MainPage.xaml.cs
+++ PetPooling/PetPooling/MainPage.xaml.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace PetPooling
 {
-	public partial class MainPage : ContentPage
+    public partial class MainPage : ContentPage
 	{
 		public MainPage()
 		{
+            
 			InitializeComponent();
 		}
 	}
