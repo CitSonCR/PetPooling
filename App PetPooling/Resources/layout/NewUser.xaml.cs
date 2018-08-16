adfa0bcfc20fa883276688857c300003a75018ba
Marlon <mrojasc490@ulacit.ed.cr>
2018-08-16 00:13:31 -0600

    Registro user 1.0

---
+++ App PetPooling/Resources/layout/NewUser.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace App_PetPooling.Resources.layout
+{
+    using Xamarin.Forms;
+
+      
+    public partial class NewUser : ContentPage
+    {
+
+        public NewUser()
+        {
+            InitializeComponent();
+        }
+    }
+}
