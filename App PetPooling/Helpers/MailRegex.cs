63039671af540e1e827f22e92ff3ab2591451cdd
Marlon <mrojasc490@ulacit.ed.cr>
2018-08-16 00:13:31 -0600

    Registro user 1.0

---
+++ App PetPooling/Helpers/MailRegex.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace App_PetPooling.Helpers
+{
+    class MailRegex
+    {
+
+        public static class RegexUtilities
+        {
+            public static bool IsValidEmail(string email)
+            {
+                var expresion = "\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*";
+                if (Regex.IsMatch(email, expresion))
+                {
+                    if (Regex.Replace(email, expresion, String.Empty).Length == 0)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
---
+++ App PetPooling/Usuarios/Registration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace App_PetPooling.Usuarios
+{
+    class Registration
+    {
+
+        public string name { get; set; }
+        public int phone { get; set; }
+        public string email { get; set; }
+        public string code { get; set; }
+        public bool car { get; set; }
+        public string password { get; set; }
+        public string passwordConfirmation { get; set; }
+
+    }
+}
